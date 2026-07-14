22942fbb30ad13ef36aa9edb2f79bbae6bc1a892
JxCl-L <jiaxili0624@gmail.com>
2026-07-14 20:39:55 +1000

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        record = {}
+        for s in strs:
+            s_sort = "".join(sorted(s))
+            if s_sort in record:
+                record[s_sort].append(s)
+            else:
+                record[s_sort] = [s]
+            
+        return list(record.values)
+        
